a95420483e8fa1265170a3b21c71a79927e41ed7
Vincent Dandenault <vdandenault@gmail.com>
2021-03-14 11:21:23 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,71 @@
+import torch
+
+class SAM(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, rho=0.05, **kwargs):
+        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+
+        defaults = dict(rho=rho, **kwargs)
+        super(SAM, self).__init__(params, defaults)
+
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+        self.param_groups = self.base_optimizer.param_groups
+
+    @torch.no_grad()
+    def first_step(self, zero_grad=False):
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            scale = group["rho"] / (grad_norm + 1e-12)
+
+            for p in group["params"]:
+                if p.grad is None: continue
+                e_w = p.grad * scale.to(p)
+                p.add_(e_w)  # climb to the local maximum "w + e(w)"
+                self.state[p]["e_w"] = e_w
+
+        if zero_grad: self.zero_grad()
+
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None: continue
+                p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
+
+        self.base_optimizer.step()  # do the actual "sharpness-aware" update
+
+        if zero_grad: self.zero_grad()
+
+
+    def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
+        norm = torch.norm(
+                    torch.stack([
+                        p.grad.norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                    p=2
+               )
+        return norm
+      
+      
+      
+from sam import SAM
+def main(): 
+
+
+  model = YourModel()
+  base_optimizer = torch.optim.SGD  # define an optimizer for the "sharpness-aware" update
+  optimizer = SAM(model.parameters(), base_optimizer, lr=0.1, momentum=0.9)
+
+
+  for input, output in data:
+
+    # first forward-backward pass
+    loss = loss_function(output, model(input))  # use this loss for any training statistics
+    loss.backward()
+    optimizer.first_step(zero_grad=True)
+  
+  # second forward-backward pass
+    loss_function(output, model(input)).backward()  # make sure to do a full forward pass
+    optimizer.second_step(zero_grad=True)
